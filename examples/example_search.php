9825a6ed251fc9fe757d05dca0cdf2897ef47994
Wesley Cintra do Nascimento <wesley.nascimento@mercadolivre.com>
2013-01-11 15:30:40 -0200

    Arquivos de exemplos padronizados

--- examples/example_search.php
+++ examples/example_search.php
@@ -1,42 +1,44 @@
 <?php
-
 require '../src/meli.php';
 
 // Create our Application instance (replace this with your appId and secret).
 $meli = new Meli(array(
-	'appId'  	=> getenv('MeliPHPAppId'),
-	'secret' 	=> getenv('MeliPHPSecret'),
+	'appId'  	=> 'MeliPHPAppId',
+	'secret' 	=> 'MeliPHPSecret',
 ));
 
-if(isset($_REQUEST['q'])){
+if(isset($_REQUEST['q'])):
 	
 	$query = $_REQUEST['q'];
 	
 	$search = $meli->get('/sites/#{siteId}/search',array(
-	'q' => $query,
-	));
+		'q' => $query)
+	);
 	
-}
+endif;
 ?>
 <!doctype html>
 <html>
-  <head>
+<head>
 	<meta charset="UTF-8"/>
-    <title>MeliPHP SDK - Example Search</title>
-  </head>
-  <body>
-  	<h1>MeliPHP SDK - Example Search</h1>
+   	<title>MeliPHP SDK - Example Search</title>
+</head>
+<body>
+	
+	<h1>MeliPHP SDK - Example Search</h1>
     
     <form>
     	<input name="q" value="<?php echo $query; ?>" />
     	<input type="submit" name="search" value="search"/>
     </form>
+	
 	<ol>
 	<?php
-		foreach ($search['json']['results'] as &$searchItem) {
+		foreach ($search['json']['results'] as &$searchItem):
 		   echo '<li><a href="' . $searchItem['permalink'] . '">'. $searchItem['title'].'</a></li>';
-		}
+		endforeach;
 	?>
     </ol>
-  </body>
+
+</body>
 </html>

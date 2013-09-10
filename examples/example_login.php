941334352fa61ca66edb226311939e8067c650ae
Bruno Elia <bruno@elia.com.br>
2013-09-10 18:15:28 -0300

    New example oAuth PHP

--- examples/example_login.php
+++ examples/example_login.php
@@ -21,10 +21,10 @@ if($_GET['code'] || $_SESSION['access_token']) {
 		if($_SESSION['expires_in'] < time()) {
 			try {
 				// Make the refresh proccess
-			    $refresh = $meli->refreshAccessToken();
+				$refresh = $meli->refreshAccessToken();
 
-			    // Now we create the sessions with the new parameters
-			    $_SESSION['access_token'] = $refresh['body']->access_token;
+				// Now we create the sessions with the new parameters
+				$_SESSION['access_token'] = $refresh['body']->access_token;
 				$_SESSION['expires_in'] = time() + $refresh['body']->expires_in;
 				$_SESSION['refresh_token'] = $refresh['body']->refresh_token;
 			} catch (Exception $e) {

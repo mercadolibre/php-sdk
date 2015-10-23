5f58d6225e8f07ad85a536878778279386085208
Phaael <rapha_r9@hotmail.com>
2015-10-23 14:12:36 -0200

    fix array sites

--- Meli/meli.php
+++ Meli/meli.php
@@ -14,19 +14,19 @@ class Meli {
     protected static $API_ROOT_URL = "https://api.mercadolibre.com";
     protected static $OAUTH_URL    = "/oauth/token";
     public static $AUTH_URL = array(
-        MLA => "https://auth.mercadolibre.com.ar", // Argentina 
-        MLB => "https://auth.mercadolivre.com.br", // Brasil
-        MCO => "https://auth.mercadolibre.com.co", // Colombia
-        MCR => "https://auth.mercadolibre.com.cr", // Costa Rica
-        MEC => "https://auth.mercadolibre.com.ec", // Ecuador
-        MLC => "https://auth.mercadolibre.com.cl", // Chile
-        MLM => "https://auth.mercadolibre.com.mx", // Mexico
-        MLU => "https://auth.mercadolibre.com.uy", // Uruguay
-        MLV => "https://auth.mercadolibre.com.ve", // Venezuela
-        MPA => "https://auth.mercadolibre.com.pa", // Panama
-        MPE => "https://auth.mercadolibre.com.pe", // Peru
-        MPT => "https://auth.mercadolibre.com.pt", // Prtugal
-        MRD => "https://auth.mercadolibre.com.do"  // Dominicana
+        "MLA" => "https://auth.mercadolibre.com.ar", // Argentina 
+        "MLB" => "https://auth.mercadolivre.com.br", // Brasil
+        "MCO" => "https://auth.mercadolibre.com.co", // Colombia
+        "MCR" => "https://auth.mercadolibre.com.cr", // Costa Rica
+        "MEC" => "https://auth.mercadolibre.com.ec", // Ecuador
+        "MLC" => "https://auth.mercadolibre.com.cl", // Chile
+        "MLM" => "https://auth.mercadolibre.com.mx", // Mexico
+        "MLU" => "https://auth.mercadolibre.com.uy", // Uruguay
+        "MLV" => "https://auth.mercadolibre.com.ve", // Venezuela
+        "MPA" => "https://auth.mercadolibre.com.pa", // Panama
+        "MPE" => "https://auth.mercadolibre.com.pe", // Peru
+        "MPT" => "https://auth.mercadolibre.com.pt", // Prtugal
+        "MRD" => "https://auth.mercadolibre.com.do"  // Dominicana
     );
 
     /**

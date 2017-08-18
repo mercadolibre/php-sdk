2a725240541536edec31dc314ebdfd60d3a3d7db
Martin Diaz <martin.diaz@mercadolibre.com>
2017-08-18 14:30:42 -0300

    Add test for Heroku app in PHP

---
+++ configApp.php
@@ -0,0 +1,13 @@
+<?php
+
+/* Esto es un comentario multilínea y otra lína de comentarios */
+$appId = getenv('App_ID');
+
+/* Esto es un comentario multilínea y otra lína de comentarios */
+$secretKey = getenv('Secret_Key');
+
+/* Esto es un comentario multilínea y otra lína de comentarios */
+$redirectURI = getenv('Redirect_URI');
+
+/* Esto es un comentario multilínea y otra lína de comentarios */
+$countryApp = 'MLA';
--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+session_start();
+require 'Meli/meli.php';
+require 'configApp.php';
+
+
 
 echo '<a href="examples/example_login.php"><b>Login using MercadoLibre oAuth 2.0</b></a><br>';
 echo '<a href="examples/example_get.php"><b>GET using MercadoLibre oAuth 2.0</b></a><br>';
@@ -7,21 +12,54 @@ echo '<a href="examples/example_put_description.php"><b>PUT using MercadoLibre o
 echo '<a href="examples/example_delete_question.php"><b>Delete question using MercadoLibre oAuth 2.0</b></a><br>';
 
 echo "<br>App_ID<br>";
-echo getenv('App_ID');
+echo $appId;
 
 echo "<br>Secret_Key<br>";
-echo getenv('Secret_Key');
+echo $secretKey
 
 echo "<br>Redirect_URI<br>";
-echo getenv('Redirect_URI');
+echo $redirectURI;
 
-echo "<br><br>";
-include_once "examples/example_delete_question.php";
-echo "<br><br>";
-include_once "examples/example_get.php";
-echo "<br><br>";
-include_once "examples/example_list_item.php";
-echo "<br><br>";
-include_once "examples/example_login.php";
-echo "<br><br>";
-include_once "examples/example_put_description.php";
+echo "<br>Site App<br>";
+echo $countryApp;
+
+
+
+$meli = new Meli($appId, $secretKey);
+$redirectURI = $redirectURI;
+
+if($_GET['code'] || $_SESSION['access_token']) {
+
+	// If code exist and session is empty
+	if($_GET['code'] && !($_SESSION['access_token'])) {
+		// If the code was in get parameter we authorize
+		$user = $meli->authorize($_GET['code'], $redirectURI);
+		
+		// Now we create the sessions with the authenticated user
+		$_SESSION['access_token'] = $user['body']->access_token;
+		$_SESSION['expires_in'] = time() + $user['body']->expires_in;
+		$_SESSION['refresh_token'] = $user['body']->refresh_token;
+	} else {
+		// We can check if the access token in invalid checking the time
+		if($_SESSION['expires_in'] < time()) {
+			try {
+				// Make the refresh proccess
+				$refresh = $meli->refreshAccessToken();
+
+				// Now we create the sessions with the new parameters
+				$_SESSION['access_token'] = $refresh['body']->access_token;
+				$_SESSION['expires_in'] = time() + $refresh['body']->expires_in;
+				$_SESSION['refresh_token'] = $refresh['body']->refresh_token;
+			} catch (Exception $e) {
+			  	echo "Exception: ",  $e->getMessage(), "\n";
+			}
+		}
+	}
+
+	echo '<pre>';
+		print_r($_SESSION);
+	echo '</pre>';
+	
+} else {
+	echo '<a href="' . $meli->getAuthUrl($redirectURI, Meli::$AUTH_URL[$countryApp]) . '">Login using MercadoLibre oAuth 2.0</a>';
+}

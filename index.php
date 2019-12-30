63219859802afe5020275f372246093bb8d0129d
Martin Diaz <diaz.martin@gmail.com>
2019-12-30 18:25:29 -0300

    Revert "Fix refresh token in demo page"

--- index.php
+++ index.php
@@ -169,29 +169,20 @@ $appName = explode('.', $domain)[0];
 
                     if($_GET['code'] && $_GET['publish_item']) {
 
-                        // If code exist and session is empty
-                        if($_GET['code'] && !($_SESSION['access_token'])) {
-                            // If the code was in get parameter we authorize
-                            $user = $meli->authorize($_GET['code'], $redirectURI);
+                        // If the code was in get parameter we authorize
+                        $user = $meli->authorize($_GET['code'], $redirectURI);
 
-                            // Now we create the sessions with the authenticated user
-                            $_SESSION['access_token'] = $user['body']->access_token;
-                            $_SESSION['expires_in'] = time() + $user['body']->expires_in;
-                            $_SESSION['refresh_token'] = $user['body']->refresh_token;
-                        } else {
-                            // We can check if the access token in invalid checking the time
-                            if($_SESSION['expires_in'] < time()) {
-                                try {
-                                    // Make the refresh proccess
-                                    $refresh = $meli->refreshAccessToken();
+                        // Now we create the sessions with the authenticated user
+                        $_SESSION['access_token'] = $user['body']->access_token;
+                        $_SESSION['expires_in'] = $user['body']->expires_in;
+                        $_SESSION['refresh_token'] = $user['body']->refresh_token;
 
-                                    // Now we create the sessions with the new parameters
-                                    $_SESSION['access_token'] = $refresh['body']->access_token;
-                                    $_SESSION['expires_in'] = time() + $refresh['body']->expires_in;
-                                    $_SESSION['refresh_token'] = $refresh['body']->refresh_token;
-                                } catch (Exception $e) {
-                                    echo "Exception: ",  $e->getMessage(), "\n";
-                                }
+                        // We can check if the access token in invalid checking the time
+                        if($_SESSION['expires_in'] + time() + 1 < time()) {
+                            try {
+                                print_r($meli->refreshAccessToken());
+                            } catch (Exception $e) {
+                                echo "Exception: ",  $e->getMessage(), "\n";
                             }
                         }
 

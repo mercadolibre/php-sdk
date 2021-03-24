104c3fcd7db1e353735abeee7e0de27182dec231
Franco Sabolo <franco.sabolo@wearetonica.com>
2021-03-24 01:32:03 +0100

    #remove query params access token

--- lib/Api/ItemsApi.php
+++ lib/Api/ItemsApi.php
@@ -610,19 +610,6 @@ class ItemsApi
         $httpBody = '';
         $multipart = false;
 
-        // query params
-        if ($access_token !== null) {
-            if('form' === 'form' && is_array($access_token)) {
-                foreach($access_token as $key => $value) {
-                    $queryParams[$key] = $value;
-                }
-            }
-            else {
-                $queryParams['access_token'] = $access_token;
-            }
-        }
-
-
         // path params
         if ($id !== null) {
             $resourcePath = str_replace(
